99e978c066f713c8156509d7ea0069fbfcfd6650
ManWithA1000Names <loena.phivos2010@gmail.com>
2023-01-20 04:58:10 +0200

    some minor updates

--- config.fish
+++ config.fish
@@ -2,3 +2,7 @@ if status is-interactive
     starship init fish | source
     zoxide init fish | source
 end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
