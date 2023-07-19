68adec4193b5b33356f473a54e683e65a0833036
ManWithA1000Names <loena.phivos2010@gmail.com>
2023-07-19 16:18:54 +0300

    add rm / new / now functions in shell.fish

--- conf.d/shell.fish
+++ conf.d/shell.fish
@@ -7,6 +7,43 @@ function ckdir
     cd $argv[1]
 end
 
+alias rm "rm -rf"
+function new
+  if test -z $argv[1]
+    echo 'Create nested files and directories'
+    echo 'If the input ends with a / is it assumed to be a directory other wise it is a file.'
+    echo ""
+    echo 'USAGE: new <input>'
+    return 1;
+  end
+  for arg in $argv
+    if string match --regex '/$' "$arg" > /dev/null
+      mkdir -p "$arg"  
+    else
+      mkdir -p (dirname "$arg")
+      touch "$arg"
+    end
+  end
+end
+
+function now
+  if test -z "$argv[1]"
+    echo "Create nested files and directories and then cd/vim the created path"
+    echo "If the input ends with a / is it assumed to be a directory other wise it is a file."
+    echo ""
+    echo "USAGE: now <input>"
+    return 1;
+  end
+  if not new "$argv[1]"
+    return 1
+  end
+  if string match --regex '/$' "$argv[1]" > /dev/null
+    cd "$argv[1]"
+  else
+    vim "$argv[1]"
+  end
+end
+
 function add_var
   if test -z $argv[1] || test -z $argv[2]
     echo "please use this syntax: 'NAME' 'VALUE'"
