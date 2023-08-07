3731eb72c29ff9d092de724e42030917b35567f3
ManWithA1000Names <loena.phivos2010@gmail.com>
2023-08-07 14:29:38 +0300

    add vi mode

--- conf.d/keybinds.fish
+++ conf.d/keybinds.fish
@@ -1,8 +1,17 @@
- bind ";" accept-autosuggestion
- bind \ek history-search-backward
- bind \ej history-search-forward
+ bind -M insert \; accept-autosuggestion
+ bind -M default \; accept-autosuggestion
+ bind -M insert -m default \cj\ck repaint
+ 
 
- bind \ed peco_select_cd
- bind \ez peco_select_z
- bind \eh peco_select_history
- bind \ef peco_select_file
+ bind -M insert \ca beginning-of-line
+ bind -M insert \ce end-of-line
+
+ bind -M default \ed peco_select_cd
+ bind -M default \ez peco_select_z
+ bind -M default \eh peco_select_history
+ bind -M default \ef peco_select_file
+
+ bind -M insert \ed peco_select_cd
+ bind -M insert \ez peco_select_z
+ bind -M insert \eh peco_select_history
+ bind -M insert \ef peco_select_file
