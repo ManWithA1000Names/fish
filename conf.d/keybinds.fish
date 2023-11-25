1245bb46b7345324f087601c3508e2f83a4e4d01
ManWithA1000Names <loena.phivos2010@gmail.com>
2023-11-25 05:48:40 +0200

    remove fnm and clean up keybinds

--- conf.d/keybinds.fish
+++ conf.d/keybinds.fish
@@ -1,17 +1,18 @@
- bind -M insert \; accept-autosuggestion
- bind -M default \; accept-autosuggestion
- bind -M insert -m default \cj\ck repaint
- 
+ bind \; accept-autosuggestion
 
- bind -M insert \ca beginning-of-line
- bind -M insert \ce end-of-line
+ bind \ek history-search-backward
+ bind \ej history-search-forward
 
- bind -M default \ed peco_select_cd
- bind -M default \ez peco_select_z
- bind -M default \eh peco_select_history
- bind -M default \ef peco_select_file
 
- bind -M insert \ed peco_select_cd
- bind -M insert \ez peco_select_z
- bind -M insert \eh peco_select_history
- bind -M insert \ef peco_select_file
+ # bind -M insert \ca beginning-of-line
+ # bind -M insert \ce end-of-line
+
+ # bind -M default \ed peco_select_cd
+ # bind -M default \ez peco_select_z
+ # bind -M default \eh peco_select_history
+ # bind -M default \ef peco_select_file
+
+ bind \ed peco_select_cd
+ bind \ez peco_select_z
+ bind \eh peco_select_history
+ bind \ef peco_select_file
