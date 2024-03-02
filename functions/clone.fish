ceb694908902c0b67b852a1c18bed369bbe611ac
ManWithA1000Names <loena.phivos2010@gmail.com>
2024-03-02 16:35:17 +0200

    add printing to clone function

--- functions/clone.fish
+++ functions/clone.fish
@@ -5,14 +5,18 @@ function clone
   end
   if string match 'http*' $argv[1]
     if test "$argv[2]" = "full"
+      echo "Executing: git clone $argv[1]"
       git clone $argv[1]
     else
+      echo "Executing: git clone $argv[1] --depth 1"
       git clone $argv[1] --depth 1
     end
   else
     if test "$argv[2]" = "full"
+      echo "Executing: gh repo clone $argv[1]"
       gh repo clone $argv[1]
     else
+      echo "Executing: gh repo clone $argv[1] -- --depth 1"
       gh repo clone $argv[1] -- --depth 1
     end
   end
