e4f4e645d472fdad94d5a9dfcd976cc492abb9b7
ManWithA1000Names <loena.phivos2010@gmail.com>
2024-10-02 22:54:24 +0300

    mak ned put the cursor on the last insert mode place and center the screen

--- functions/ned.fish
+++ functions/ned.fish
@@ -1,7 +1,7 @@
 function ned
   if ! test -e "flake.nix"
     echo "flake.nix: No such file or directory."
-    exit 1
+    return 1
   end
 
   if set root (git rev-parse --show-toplevel)
@@ -10,5 +10,5 @@ function ned
   else
     set last_file (find . -type f -exec stat -c "%Y %n" {} \; | sort -nr | head -1 | awk '{print $2}')
   end
-  nix develop -c $EDITOR $last_file
+  nix develop -c $EDITOR $last_file -c 'normal! `^zz'
 end
