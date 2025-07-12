95dfafa1d7955faac299bd6e8f863df9bbba6b0a
ManWithA1000Names <loena.phivos2010@gmail.com>
2025-07-12 17:47:31 +0300

    remove direnv, add $NOX_* vars

--- conf.d/set.fish
+++ conf.d/set.fish
@@ -1,4 +1,8 @@
 set -x EDITOR "vix"
+
 set -x NOX_EDITOR "zed"
+set -x NOX_EDITOR_ARGS "."
+
 set -x NOX_TERM_EDITOR "vix"
+
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
--- config.fish
+++ config.fish
@@ -1,5 +1,4 @@
 if status is-interactive
     starship init fish | source
     zoxide init fish | source
-    direnv hook fish | source
 end
